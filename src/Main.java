5217f8324a42586924c63020606e0aaab1b30ea1
79535 <twinm124@gmail.com>
2023-10-28 12:37:15 +0300

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+
+        //Задача 2
+        System.out.println("Задача 2");
+        //Задача 3
+        System.out.println("Задача 3");
+        //Задача 4
+        System.out.println("Задача 4");
     }
 }
